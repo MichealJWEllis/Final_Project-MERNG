5e00196dd34245a763c113513324f5b36aa64cdf
mellis <mellis.eriecon@gmail.com>
2021-07-22 09:43:14 -0400

    file managment

--- server/server.js
+++ server/server.js
@@ -2,9 +2,11 @@ const express = require('express');
 const { ApolloServer } = require('apollo-server-express')
 const { gql } = require("apollo-server-express");
 
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 const path = require('path');
+const db = require('./config/connection');
 
 const typeDefs = gql`
     type Query{
@@ -29,9 +31,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 
-app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
-    // log where we can go to test our GQL API
-    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+db.once('open', () => {
+    app.listen(PORT, () => {
+        console.log(`API server running on port ${PORT}!`);
+        // log where we can go to test our GQL API
+        console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    });
 });
-
