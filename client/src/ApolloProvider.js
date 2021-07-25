043749d4c0073cfd4003fb97f4f4c7175980cfbe
michealjwellis <mellis.eriecon@gmail.com>
2021-07-25 01:44:36 -0400

    apollo setup

--- client/src/ApolloProvider.js
+++ client/src/ApolloProvider.js
@@ -6,7 +6,7 @@ import { createHttpLink } from '@apollo/client'
 import { ApolloProvider } from '@apollo/client'
 
 const httpLink = createHttpLink({
-    uri: 'http://localhost:3001'
+    uri: 'http://localhost:3001/graphql'
 })
 const client = new ApolloClient({
     link: httpLink,
