2bd8b6c6d9172fd4ab5e3a9e969fa7833c288fe2
michealjwellis <mellis.eriecon@gmail.com>
2021-07-26 01:31:01 -0400

    fail

---
+++ server/config/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    MONGODB: 'mongodb+srv://mellis-admin:FefN4qFALOexaNw8@cluster0.vihvx.mongodb.net/Final_Project?retryWrites=true&w=majority',
+    KEY: 'a key that is secret'
+}
